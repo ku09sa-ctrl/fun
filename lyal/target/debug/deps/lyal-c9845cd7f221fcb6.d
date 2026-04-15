fa1df92c229bcbe4778e2bbc16c4d9bc0dcbad2f
ku09sa-ctrl <ku09sa@gmail.com>
2026-04-15 10:28:23 +0530

    Initial...? Commit May be ?

---
+++ lyal/target/debug/deps/lyal-c9845cd7f221fcb6.d
@@ -0,0 +1,9 @@
+/home/dada1/fun/lyal/target/debug/deps/lyal-c9845cd7f221fcb6.d: src/main.rs Cargo.toml
+
+/home/dada1/fun/lyal/target/debug/deps/liblyal-c9845cd7f221fcb6.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
